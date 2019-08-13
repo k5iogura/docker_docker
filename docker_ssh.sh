101dd13f12d6fbbd2c4216800373524e6f90aff0
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-08-13 22:41:22 +0900

    revise

--- docker_ssh.sh
+++ docker_ssh.sh
@@ -1,1 +1,2 @@
-docker run -d --net host -e DISPLAY=$DISPLAY -p 10000:22 -v $HOME/.Xauthority:/root/.Xauthority ssh_server
+docker run -d -p 10000:22 ssh_server
+#docker run -d --net host -e DISPLAY=$DISPLAY -p 10000:22 -v $HOME/.Xauthority:/root/.Xauthority ssh_server
